87c12c21237f3b3c5b380d6bec72797687063f30
Utkarsh Bele <utkarshbele@gmail.com>
2024-09-19 00:21:14 +0530

    basic

---
+++ Java/Spring_SpringBoot/Student_Management_System/src/mang_sys/Students.java
@@ -0,0 +1,15 @@
+package mang_sys;
+
+public class Students {
+    private String id, enroll,drop, fees;
+
+    public String getId() {
+        return id;
+    }
+    public String getFees() {
+        return fees;
+    }
+    public void enrolledStudents(){}
+    public void dropStudents(){}
+
+}
---
+++ Java/Spring_SpringBoot/Student_Management_System/src/mang_sys/Teachers.java
@@ -0,0 +1,21 @@
+package mang_sys;
+
+public class Teachers {
+    private String name, dept, hours, subjects;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public String getHours() {
+        return hours;
+    }
+
+    public String getSubjects() {
+        return subjects;
+    }
+}
